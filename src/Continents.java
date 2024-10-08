cc3027386a98fcdae4b975c13638b5cdaf89a3bf
Javier Trujillo <rivera.dev@outlook.com>
2024-10-08 00:17:44 -0600

    Added - switch

--- src/Continents.java
+++ src/Continents.java
@@ -1,11 +1,36 @@
 public class Continents {
+    int continent = 4;
+
+
+    public String continentIdentification(){
+        switch (continent){
+            case 1:
+                return "North America: Mexico City, Mexico";
+                break;
+            case 2:
+                return "South America: Sao Paulo, Brazil";
+                break;
+            case 3:
+                return "Europe: Moscow, Russia";
+                break;
+            case 4:
+                return "Africa: Lagos, Nigeria";
+                break;
+            case 5:
+                return "Asia: Shanghai, China";
+                break;
+            case 6:
+                return "Australia: Sydney, Australia";
+                break;
+            case 7:
+                return "Antarctica: McMurdo Station, US";
+                break;
+            default:
+                return "Undefined continent!";
+        }
+    }
 
     public static void main(String[] args){
-        int continent = 4;
 
-        switch (continent){
-
-            case
-        }
     }
 }
