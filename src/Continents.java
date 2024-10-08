958e2451f4ef9d42849226249f41490688eacbd8
Javier Trujillo <rivera.dev@outlook.com>
2024-10-08 00:27:50 -0600

    Fixed - breaks from switch not necesary, return statement ends switch cases

--- src/Continents.java
+++ src/Continents.java
@@ -10,25 +10,18 @@ public class Continents {
         switch (continent){
             case 1:
                 return "North America: Mexico City, Mexico";
-                //break;
             case 2:
                 return "South America: Sao Paulo, Brazil";
-                //break;
             case 3:
                 return "Europe: Moscow, Russia";
-                //break;
             case 4:
                 return "Africa: Lagos, Nigeria";
-                //break;
             case 5:
                 return "Asia: Shanghai, China";
-                //break;
             case 6:
                 return "Australia: Sydney, Australia";
-                //break;
             case 7:
                 return "Antarctica: McMurdo Station, US";
-                //break;
             default:
                 return "Undefined continent!";
         }
